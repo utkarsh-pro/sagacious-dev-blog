495b6ab7727dd9c4e979493b625d18e0a7136c98
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-04-29 00:22:02 +0530

    Fixed stale contentstate value issue

--- src/components/BlogEditor/CodeEditorWrapper/index.tsx
+++ src/components/BlogEditor/CodeEditorWrapper/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback, useRef } from 'react'
 import Editor from '../../Editor'
 
-// =================================== INTERFACES =========================================
+// =================================== INTERFACES ========================================
+
 export interface ICodeEditorWrapper {
     blockProps: {
         language: string;
@@ -23,11 +24,17 @@ const CodeEditorWrapper = (props: ICodeEditorWrapper) => {
     const { blockProps, block, contentState } = props;
     const [init, setInit] = useState({ code: "", language: blockProps.language })
 
+    // Had to hold a reference to the latest value of contentstate
+    // as for some reason the callback functions were getting stale
+    // value of the this prop
+    const contentStateRef = useRef<any>(contentState)
+    contentStateRef.current = contentState;
+
+    // This sets the initial code while also making sure
+    // that no "code" prop of the Editor is not
+    // directly associated with a wrapper prop
+    // Hence it avoids rerenders on each click
     useEffect(() => {
-        // This sets the initial code while also making sure
-        // that no "code" prop of the Editor is not
-        // directly associated with a wrapper prop
-        // Hence it avoids rerenders on each click
         const entityKey = block.getEntityAt(0);
 
         if (entityKey) {
@@ -38,6 +45,7 @@ const CodeEditorWrapper = (props: ICodeEditorWrapper) => {
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
 
     /**
      * Memoized implementation of onBlur handler
@@ -53,26 +61,21 @@ const CodeEditorWrapper = (props: ICodeEditorWrapper) => {
         blockProps.setEditorIsUp(true)
     }, [blockProps])
 
-    /**
-     * Implementation of onChange handler
-     */
-    const onChange = (code: string, language?: string) => {
-        const entityKey = block.getEntityAt(0);
-        if (entityKey) {
-            const newContentState = contentState.mergeEntityData(
-                entityKey,
-                { content: code, language: language || blockProps.language }
-            )
-            console.log(language)
-            blockProps.onFinishEdit(newContentState)
-        }
-    }
-
     return (
         <Editor
             header
             footer
-            onChange={onChange}
+            onChange={(code: string, language?: string) => {
+                const entityKey = block.getEntityAt(0);
+                const contentState = contentStateRef.current;
+                if (entityKey) {
+                    const newContentState = contentState.mergeEntityData(
+                        entityKey,
+                        { content: code, language: language || blockProps.language }
+                    )
+                    blockProps.onFinishEdit(newContentState)
+                } else console.log("OOPS")
+            }}
             code={init.code}
             onBlur={memoizedOnBlur}
             onFocus={memoizedOnFocus}
